25e232ecd73f996d7c5eea9806cad2023c1c5ed4
KikirrinHC <enrique.hdez.cortes@gmail.com>
2022-08-16 19:35:25 -0500

    Primera revisión

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\myfirstController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [myfirstController::class, 'test']);
